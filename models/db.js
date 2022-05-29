74687a8e0f137ffec1745de3221b8069b88f17d7
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-05-29 03:26:23 -0700

    Fix issues with .env file

--- models/db.js
+++ models/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const connectDB = async () => {
+  console.log("trying to connect to : ", process.env.MANGO_URI)
   try {
     await
       mongoose.connect(process.env.MANGO_URI, {})
